68a63d7b780e205558adbe7b649a7337731596e2
HeumHeum2 <jwh6295@gmail.com>
2021-08-01 20:32:28 +0900

    feat: 3-4 getter, setter 사용해보기

--- 3-OOP/3-3-encapsulation.ts
+++ 3-OOP/3-3-encapsulation.ts
@@ -40,4 +40,24 @@
 
   const maker = CoffeeMaker.makeMachine(32);
   maker.fillCoffeeBeans(32);
+
+  class User {
+    get fullName(): string {
+      return `${this.firstName} ${this.lastName}`;
+    }
+    private internalAge = 4;
+    get age(): number {
+      return this.internalAge;
+    }
+    set age(num: number) {
+      if (num < 0) {
+      }
+      this.internalAge = num;
+    }
+    constructor(private firstName: string, private lastName: string) {}
+  }
+
+  const user = new User("Steve", "Jobs");
+  user.age = 6;
+  console.log(user.fullName);
 }
